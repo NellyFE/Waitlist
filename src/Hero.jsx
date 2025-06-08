46fe24704c8a14203abe2d6871255b1a16dc7257
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2025-06-08 16:27:55 +0100

    discount added

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = () => {
       <div className="flex flex-col items-center justify-center  h-[50vh]  ">
 
 
-        <h1 className="font-bold text-4xl md:text-5xl mb-6">Be the First to Know!</h1>
+        <h1 className="font-bold text-4xl text-center md:text-5xl mb-6">Be the First to Know and get <span className="text-[#4400c7]">20%</span> Off!</h1>
         <p className="text-center w-full md:w-1/2 px-6">
           We're ready to launch! Sign up for early access to Apex Tech online
           Academy 
