ef629ee673858ddb06b3c7a70f3fb0b4c6c9dd0a
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2025-06-14 06:28:28 +0100

    UI changes

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,13 +2,22 @@ export const Hero = () => {
   return (
     <section>
       <div className="flex flex-col items-center justify-center  h-[50vh]  ">
-        <h1 className="font-bold text-4xl text-center md:text-5xl mb-6">
-          Be the First to Know and get{" "}
-          <span className="text-[#4400c7]">20%</span> Off!
+        <h1 className="font-bold text-4xl text-center md:text-6xl mb-6">
+          Be the First to Know
+          <span className="block md:hidden">
+            {" "}
+            and Get <span className="text-[#4400c7]">20%</span> Off!
+          </span>
+          <span className="hidden md:inline">
+            <br />
+            and Get <span className="text-[#4400c7]">20%</span> Off!
+          </span>
         </h1>
+
         <p className="text-center w-full md:w-1/2 px-6">
-          We're ready to launch! Sign up for early access to Apex Tech online
-          Academy and be the first to get your hands on it when we go live.
+          We’re launching soon! Be the first to access courses at Apex Tech
+          Online Academy and enjoy an exclusive 20% discount on any course when
+          you sign up early.{" "}
         </p>
       </div>
     </section>
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,14 +2,14 @@ export const Home = () => {
   return (
     <section>
       <nav>
-        <div className="flex md:justify-between justify-center items-center mx-auto px-12 ">
+        <div className="flex justify-center items-center mx-auto px-12 ">
           <div className="items-center">
             <img src="/lms logo.png" alt="logo" className="w-56" />
           </div>
 
-          <div className="hidden md:flex bg-[#4400c7] rounded-lg py-2 px-6 text-white cursor-pointer border-2 border-transparent hover:bg-transparent hover:text-[#4400c7] hover:border-2 hover:border-[#4400c7] transition-all duration-500 ease-in-out ">
+          {/* <div className="hidden md:flex bg-[#4400c7] rounded-lg py-2 px-6 text-white cursor-pointer border-2 border-transparent hover:bg-transparent hover:text-[#4400c7] hover:border-2 hover:border-[#4400c7] transition-all duration-500 ease-in-out ">
             <a href="#">Join Waitlist</a>
-          </div>
+          </div> */}
         </div>
       </nav>
 
